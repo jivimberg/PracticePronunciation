2a50e5a6a5780e363cdb91c1cda112fc5f17e015
Juan Ignacio Vimberg <jivimberg@gmail.com>
2015-08-12 22:47:03 -0700

    Adding MacMillan attribution

---
+++ app/src/main/java/com/eightblocksaway/android/practicepronunciation/view/AboutDialog.java
@@ -0,0 +1,14 @@
+package com.eightblocksaway.android.practicepronunciation.view;
+
+import android.content.Context;
+import android.preference.DialogPreference;
+import android.util.AttributeSet;
+
+public class AboutDialog extends DialogPreference {
+
+    public AboutDialog(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        // TODO Auto-generated constructor stub
+    }
+
+}
